64829f8ac2dc2eba7f39a6f68901fe929c093349
Belonit <Belonit@ya.ru>
2021-04-17 15:33:57 +0300

    Update CaptureManager

--- src/Misc/CaptureManager.cpp
+++ src/Misc/CaptureManager.cpp
@@ -1,18 +1,20 @@
 #include "CaptureManager.h"
 
-#include <Helpers/Macro.h>
+#include <Utilities/Macro.h>
+
+#include <Ext/TechnoType/Body.h>
 #include <HouseClass.h>
-#include <BuildingClass.h>
 #include <AnimClass.h>
 
-#include "../Ext/TechnoType/Body.h"
-
-bool CaptureManager::CanCapture(CaptureManagerClass* pManager, TechnoClass* pTarget)
+bool CaptureManager::CanCapture(
+	CaptureManagerClass* pManager,
+	TechnoClass* pTarget
+)
 {
-    if (pManager->MaxControlNodes == 1)
-        return pManager->CanCapture(pTarget);
-    
-    auto pTechnoTypeExt = TechnoTypeExt::ExtMap.Find(pManager->Owner->GetTechnoType());
+	if (pManager->MaxControlNodes == 1)
+		return pManager->CanCapture(pTarget);
+
+	auto pTechnoTypeExt = TechnoTypeExt::ExtMap.Find(pManager->Owner->GetTechnoType());
 	if (pTechnoTypeExt && pTechnoTypeExt->MultiMindControl_ReleaseVictim)
 	{
 		// I hate Ares' completely rewritten things - secsome
@@ -21,165 +23,169 @@ bool CaptureManager::CanCapture(CaptureManagerClass* pManager, TechnoClass* pTar
 		pManager->MaxControlNodes -= 1;
 		return result;
 	}
-    
+
 	return pManager->CanCapture(pTarget);
 }
 
-bool CaptureManager::FreeUnit(CaptureManagerClass* pManager, TechnoClass* pTarget, bool bSilent)
+bool CaptureManager::FreeUnit
+(
+	CaptureManagerClass* pManager,
+	TechnoClass* pTarget,
+	bool bSilent
+)
 {
-    if (pTarget)
-    {
-        for (int i = pManager->ControlNodes.Count - 1; i >= 0; --i)
-        {
-            const auto pNode = pManager->ControlNodes[i];
-            if (pTarget == pNode->Unit)
-            {
-                if (pTarget->MindControlRingAnim)
-                {
-                    pTarget->MindControlRingAnim->UnInit();
-                    pTarget->MindControlRingAnim = nullptr;
-                }
+	if (pTarget)
+	{
+		for (int i = pManager->ControlNodes.Count - 1; i >= 0; --i)
+		{
+			const auto pNode = pManager->ControlNodes[i];
+			if (pTarget == pNode->Unit)
+			{
+				if (pTarget->MindControlRingAnim) {
+					pTarget->MindControlRingAnim->UnInit();
+					pTarget->MindControlRingAnim = nullptr;
+				}
 
-                if (!bSilent)
-                {
-                    int nSound = pTarget->GetTechnoType()->MindClearedSound;
+				if (!bSilent) {
+					int nSound = pTarget->GetTechnoType()->MindClearedSound;
 
-                    if (nSound == -1)
-                        nSound = RulesClass::Instance->MindClearedSound;
-                    if (nSound != -1)
-                        VocClass::PlayIndexAtPos(nSound, pTarget->GetCoords());
-                }
+					if (nSound == -1)
+						nSound = RulesClass::Instance->MindClearedSound;
 
-                // Fix : Player defeated should not get this unit.
-                auto pOriginOwner = pNode->OriginalOwner->Defeated ?
-                    HouseClass::FindNeutral() : pNode->OriginalOwner;
+					if (nSound != -1)
+						VocClass::PlayIndexAtPos(nSound, pTarget->GetCoords());
 
-                pTarget->SetOwningHouse(pOriginOwner);
-                pManager->DecideUnitFate(pTarget);
-                pTarget->MindControlledBy = nullptr;
+				}
 
-                if (pNode)
-                    GameDelete(pNode);
+				// Fix : Player defeated should not get this unit.
+				auto pOriginOwner = pNode->OriginalOwner->Defeated ?
+					HouseClass::FindNeutral() :
+					pNode->OriginalOwner;
 
-                pManager->ControlNodes.RemoveItem(i);
+				pTarget->SetOwningHouse(pOriginOwner);
+				pManager->DecideUnitFate(pTarget);
+				pTarget->MindControlledBy = nullptr;
 
-                return true;
-            }
-        }
-    }
+				if (pNode)
+					GameDelete(pNode);
 
-    return false;
+				pManager->ControlNodes.RemoveItem(i);
+
+				return true;
+			}
+		}
+	}
+
+	return false;
 }
 
-bool CaptureManager::CaptureUnit(CaptureManagerClass* pManager, TechnoClass* pTarget, 
-    bool bRemoveFirst, AnimTypeClass* pControlledAnimType)
+bool CaptureManager::CaptureUnit
+(
+	CaptureManagerClass* pManager,
+	TechnoClass* pTarget,
+	bool bRemoveFirst,
+	AnimTypeClass* pControlledAnimType
+)
 {
-    if (CaptureManager::CanCapture(pManager, pTarget))
-    {
-		if (pManager->MaxControlNodes <= 0)
-			return false;
-
+	if (CaptureManager::CanCapture(pManager, pTarget) && pManager->MaxControlNodes > 0)
+	{
 		if (!pManager->InfiniteMindControl)
 		{
-			if (pManager->MaxControlNodes == 1 && pManager->ControlNodes.Count == 1)
-				CaptureManager::FreeUnit(pManager, pManager->ControlNodes[0]->Unit);
-			else if (pManager->ControlNodes.Count == pManager->MaxControlNodes)
-				if (bRemoveFirst)
+			if (pManager->ControlNodes.Count == pManager->MaxControlNodes)
+			{
+				if (bRemoveFirst || pManager->MaxControlNodes == 1)
 					CaptureManager::FreeUnit(pManager, pManager->ControlNodes[0]->Unit);
+
+			}
 		}
 
-        auto pControlNode = GameCreate<ControlNode>();
-        if (pControlNode)
-        {
-            pControlNode->OriginalOwner = pTarget->Owner;
-            pControlNode->Unit = pTarget;
+		auto pControlNode = GameCreate<ControlNode>();
+		if (pControlNode)
+		{
+			pControlNode->OriginalOwner = pTarget->Owner;
+			pControlNode->Unit = pTarget;
 
-            pManager->ControlNodes.AddItem(pControlNode);
-            pControlNode->LinkDrawTimer.Start(RulesClass::Instance->MindControlAttackLineFrames);
+			pManager->ControlNodes.AddItem(pControlNode);
+			pControlNode->LinkDrawTimer.Start(RulesClass::Instance->MindControlAttackLineFrames);
 
-            if (pTarget->SetOwningHouse(pManager->Owner->Owner))
-            {
-                pTarget->MindControlledBy = pManager->Owner;
+			if (pTarget->SetOwningHouse(pManager->Owner->Owner))
+			{
+				pTarget->MindControlledBy = pManager->Owner;
+				pManager->DecideUnitFate(pTarget);
 
-                pManager->DecideUnitFate(pTarget);
+				auto const pBld = abstract_cast<BuildingClass*>(pTarget);
+				auto const pType = pTarget->GetTechnoType();
+				CoordStruct location = pTarget->GetCoords();
 
-                auto const pBld = abstract_cast<BuildingClass*>(pTarget);
-                auto const pType = pTarget->GetTechnoType();
-                CoordStruct location = pTarget->GetCoords();
-                
-                if (pBld)
-                    location.Z += pBld->Type->Height * Unsorted::LevelHeight;
-                else
-                    location.Z += pType->MindControlRingOffset;
+				location.Z += pBld ?
+					pBld->Type->Height * Unsorted::LevelHeight :
+					pType->MindControlRingOffset;
 
-                if (auto const pAnimType = pControlledAnimType)
-                {
-                    if (auto const pAnim = GameCreate<AnimClass>(pAnimType, location))
-                    {
-                        pTarget->MindControlRingAnim = pAnim;
-                        pAnim->SetOwnerObject(pTarget);
-                        
-                        if (pBld)
-                            pAnim->ZAdjust = -1024;
-                    }
-                }
+				if (auto const pAnimType = pControlledAnimType)
+				{
+					if (auto const pAnim = GameCreate<AnimClass>(pAnimType, location))
+					{
+						pTarget->MindControlRingAnim = pAnim;
+						pAnim->SetOwnerObject(pTarget);
 
-                return true;
-            }
+						if (pBld)
+							pAnim->ZAdjust = -1024;
 
-        }
-    }
+					}
+				}
 
-    return false;
+				return true;
+			}
+		}
+	}
+
+	return false;
 }
 
-bool CaptureManager::CaptureUnit(CaptureManagerClass* pManager, TechnoClass* pTechno, AnimTypeClass* pControlledAnimType)
+bool CaptureManager::CaptureUnit
+(
+	CaptureManagerClass* pManager,
+	TechnoClass* pTechno,
+	AnimTypeClass* pControlledAnimType
+)
 {
-    if (pTechno)
-    {
-        const auto pTarget = pTechno->AbsDerivateID & AbstractFlags::Techno ? pTechno : nullptr;
+	if (!pTechno) {
+		return false;
+	}
 
-        bool bRemoveFirst = false;
-        if (auto pTechnoTypeExt = TechnoTypeExt::ExtMap.Find(pManager->Owner->GetTechnoType()))
-            bRemoveFirst = pTechnoTypeExt->MultiMindControl_ReleaseVictim;
+	const auto pTarget = pTechno->AbsDerivateID & AbstractFlags::Techno ? pTechno : nullptr;
 
-        return CaptureManager::CaptureUnit(pManager, pTarget, bRemoveFirst, pControlledAnimType);
-    }
-    
-    return false;
+	bool bRemoveFirst = false;
+	if ( auto pTechnoTypeExt = TechnoTypeExt::ExtMap.Find(pManager->Owner->GetTechnoType()) )
+		bRemoveFirst = pTechnoTypeExt->MultiMindControl_ReleaseVictim;
+
+	return CaptureManager::CaptureUnit(pManager, pTarget, bRemoveFirst, pControlledAnimType);
 }
 
-void CaptureManager::DecideUnitFate(CaptureManagerClass* pManager, FootClass* pFoot)
+void CaptureManager::DecideUnitFate
+(
+	CaptureManagerClass* pManager,
+	FootClass* pFoot
+)
 {
 	// to be implemented (if needed). - secsome
 }
 
-DEFINE_HOOK(471D40, CaptureManagerClass_CaptureUnit, 7)
-{
-    GET(CaptureManagerClass*, pThis, ECX);
-    GET_STACK(TechnoClass*, pTechno, 0x4);
+// =============================
+// Hooks
 
-    R->AL(CaptureManager::CaptureUnit(pThis, pTechno));
-
-    return 0x471D5A;
+bool __fastcall CaptureManagerClass_CaptureUnit(CaptureManagerClass* pThis, void*, TechnoClass* pTechno) {
+	return CaptureManager::CaptureUnit(pThis, pTechno);
 }
 
-DEFINE_HOOK(471FF0, CaptureManagerClass_FreeUnit, 8)
-{
-    GET(CaptureManagerClass*, pThis, ECX);
-    GET_STACK(TechnoClass*, pTechno, 0x4);
-
-    R->AL(CaptureManager::FreeUnit(pThis, pTechno));
-
-    return 0x472006;
+bool __fastcall CaptureManagerClass_FreeUnit(CaptureManagerClass* pThis, void*, TechnoClass* pTechno) {
+	return CaptureManager::FreeUnit(pThis, pTechno);
 }
 
-DEFINE_HOOK(6FCB34, TechnoClass_CanFire_CanCapture, 6)
-{
-    GET(TechnoClass*, pThis, ESI);
-    GET(TechnoClass*, pTarget, EBP);
-
-    R->AL(CaptureManager::CanCapture(pThis->CaptureManager, pTarget));
-
-    return 0x6FCB40;
+bool __fastcall TechnoClass_CanFire_CanCapture(CaptureManagerClass* pThis, void*, TechnoClass* pTechno) {
+	return CaptureManager::CanCapture(pThis, pTechno);
 }
+
+DEFINE_POINTER_LJMP(0x471D40, CaptureManagerClass_CaptureUnit); // Replace vanilla funcion
+DEFINE_POINTER_LJMP(0x471FF0, CaptureManagerClass_FreeUnit);    // Replace vanilla funcion
+DEFINE_POINTER_CALL(0x6FCB3B, TechnoClass_CanFire_CanCapture);  // Replace one call
