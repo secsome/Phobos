14159ef2dedd9961a0bd75a279d7a0c8416a6b0c
Uranusian <mikesariitten@gmail.com>
2021-07-01 20:21:56 +0800

    Small style pass and docs fix

--- src/Ext/TechnoType/Body.h
+++ src/Ext/TechnoType/Body.h
@@ -47,9 +47,9 @@ public:
 		Nullable<int> ChronoRangeMinimum;
 		Nullable<int> ChronoDelay;
 
-		ValueableVector<AnimTypeClass*> OregatherAnims;
-		ValueableVector<int> OregatherTypes;
-		ValueableVector<int> OregatherFramesPerDir;
+		ValueableVector<AnimTypeClass*> OreGath_Anims;
+		ValueableVector<int> OreGath_TiberiumTypes;
+		ValueableVector<int> OreGath_FramesPerDir;
 
 		ExtData(TechnoTypeClass* OwnerObject) : Extension<TechnoTypeClass>(OwnerObject),
 			HealthBar_Hide(false),
@@ -79,9 +79,9 @@ public:
 			ChronoMinimumDelay(),
 			ChronoRangeMinimum(),
 			ChronoDelay(),
-			OregatherAnims(),
-			OregatherTypes(),
-			OregatherFramesPerDir()
+			OreGath_Anims(),
+			OreGath_TiberiumTypes(),
+			OreGath_FramesPerDir()
 		{ }
 
 		virtual ~ExtData() = default;
