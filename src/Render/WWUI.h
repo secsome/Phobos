9eb193920033c35523850eeeef9e5347e9dc1f9d
secsome <302702960@qq.com>
2026-05-27 20:55:39 +0800

    Replace ComboBoxProc

--- src/Render/WWUI.h
+++ src/Render/WWUI.h
@@ -8,4 +8,5 @@ namespace WWUI
 	LRESULT CALLBACK OwnerDrawWindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 	LRESULT CALLBACK ScrollBarCtrl(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 	LRESULT CALLBACK ListBoxCtrl(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+	LRESULT CALLBACK ComboBoxCtrl(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 }
