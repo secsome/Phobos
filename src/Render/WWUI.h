f257709b1b6e1c4da14e59da5ea4ce16413023b7
secsome <302702960@qq.com>
2026-05-27 23:00:58 +0800

    Replace TabCtrlProc

--- src/Render/WWUI.h
+++ src/Render/WWUI.h
@@ -14,4 +14,5 @@ namespace WWUI
 	LRESULT CALLBACK NewEditCtrl(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 	LRESULT CALLBACK EditCtrl(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 	LRESULT CALLBACK StaticCtrl(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
+	LRESULT CALLBACK TabCtrl(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 }
